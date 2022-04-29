6c8124de1d6186741b95f7cdc9c305fca5275633
Raphael Mulenda <mulendaraphael@yahoo.fr>
2022-04-29 21:37:31 +0200

    Update of BankOperation Class

--- src/BankOperation.java
+++ src/BankOperation.java
@@ -1,2 +1,51 @@
+/**
+ * Created by Raphael Mulenda on 29/04/2022.
+ */
 public class BankOperation {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("12345-678",1000.00);
+        /*Thread customer1 = new Thread(){
+
+            public void run() {
+           bankAccount.deposit(300);
+                System.out.println("Customer1" + " Balance after deposit: " + bankAccount.currentBalance() );
+           bankAccount.withdraw(50.00);
+                System.out.println("Customer1" + " Balance after withdraw: " + bankAccount.currentBalance() );
+            }
+        };
+
+        Thread customer2 = new Thread() {
+
+            public void run() {
+                bankAccount.deposit(250.45);
+                System.out.println("Customer2" + " Balance after deposit: " + bankAccount.currentBalance() );
+                bankAccount.withdraw(100.00);
+                System.out.println("Customer2" + " Balance after withdraw: " + bankAccount.currentBalance() );
+            }
+        } ;*/
+
+        Thread customer1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bankAccount.deposit(300);
+                System.out.println("Customer1" + " Balance after deposit: " + bankAccount.currentBalance() );
+                bankAccount.withdraw(50.00);
+                System.out.println("Customer1" + " Balance after withdraw: " + bankAccount.currentBalance() );
+            }
+        });
+
+        Thread customer2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bankAccount.deposit(250.45);
+                System.out.println("Customer2" + " Balance after deposit: " + bankAccount.currentBalance() );
+                bankAccount.withdraw(100.00);
+                System.out.println("Customer2" + " Balance after withdraw: " + bankAccount.currentBalance() );
+            }
+        });
+
+        customer1.start();
+        customer2.start();
+    }
+
 }
